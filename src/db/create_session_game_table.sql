922e01508094ceb6a5655e68d7fbf325ad152a50
clayton <claytonphillipsdorsett@gmail.com>
2022-10-02 18:55:58 -0400

    Remove delete on cascade

--- src/db/create_session_game_table.sql
+++ src/db/create_session_game_table.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS session_game
     id         INT AUTO_INCREMENT PRIMARY KEY,
     session_id TINYTEXT NOT NULL,
     game_id    INT      NOT NULL,
-    FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE
+    FOREIGN KEY (game_id) REFERENCES game (id)
 );
